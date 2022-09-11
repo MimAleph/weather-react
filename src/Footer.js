f0336785ee00cd8a321856ac584fca458078119c
MimAleph <akbarzadeh.mim@gmail.com>
2022-09-12 01:17:28 +0430

    add date

--- src/Footer.js
+++ src/Footer.js
@@ -3,22 +3,22 @@ import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div class="footer">
+    <div className="footer">
       <a
-        class="github-link"
+        className="github-link"
         href="https://github.com/MimAleph/weather-react"
         target="_blank"
       >
         Open-source code
       </a>
       , by{" "}
-      <a class="coder" href="https://linktr.ee/MaryamAleph" target="_blank">
+      <a className="coder" href="https://linktr.ee/MaryamAleph" target="_blank">
         {" "}
         Maryam Aleph
       </a>{" "}
       and hosted on{" "}
       <a
-        class="netlify-link"
+        className="netlify-link"
         href="https://voluble-macaron-ed03d0.netlify.app/"
         target="_blank"
       >
--- src/Weather.js
+++ src/Weather.js
@@ -1,34 +1,35 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
 import "./Weather.css";
 
-export default function Search() {
+export default function Weather(props) {
   let [data, setData] = useState({ ready: false });
 
   function DisplayWeather(response) {
     setData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: Math.round(response.data.main.temp),
-      city: response.data.name,
       humidity: Math.round(response.data.main.humidity),
-      wind: Math.round(response.data.wind.speed),
+      date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      day: "Friday",
-      date: "08",
-      icon: `https://ssl.gstatic.com/onebox/weather/64/cloudy.png`,
+      icon: response.data.weather[0].icon,
+      wind: Math.round(response.data.wind.speed),
+      city: response.data.name,
     });
   }
   if (data.ready) {
     return (
       <div className="weather">
-        <form onSubmit={ShowTemperature}>
+        <form>
           <div className="row">
             <div className="col-md-9">
               <input
                 type="search"
                 placeholder="type a city"
                 className="form-control"
-                onChange={updateCity}
+                // onChange={updateCity}
               ></input>
             </div>
             <div className="col-md-3">
@@ -46,9 +47,8 @@ export default function Search() {
               <h1>{data.city}</h1>
 
               <ul>
-                <li>
-                  <span className="day">{data.day}</span>
-                  <span className="date"> {data.date}</span>
+                <li className="date">
+                  <FormattedDate date={data.date} />
                 </li>
                 <li className="description text-capitalize">
                   {data.description}
@@ -70,17 +70,14 @@ export default function Search() {
       </div>
     );
   } else {
-    let city = "New York";
-    let apiKey = "ae9cc35716eff0ddc16a6f4a0947e685";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiKey = "77fa4ec6ddf217050010d2b77d41c45d";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(DisplayWeather);
     return "Loading...";
   }
 
-  function ShowTemperature(event) {
-    event.preventDefault();
-  }
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
+  // function ShowTemperature(event) {
+  //   event.preventDefault();
+  // }
+  // function updateCity(event) {}
 }
